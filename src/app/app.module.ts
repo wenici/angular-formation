fb994c46a2bab52a433143ef2f461d9f6bb7e7a5
wenici <weni.dev@gmail.com>
2022-10-06 13:29:01 +0000

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { environment } from 'src/environments/environment';
+registerLocaleData(localeFr, 'fr');
+
+import { MaterialUiModule } from './material-ui/material-ui.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +25,17 @@ import { HeaderComponent } from './layouts/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialUiModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireAuthGuardModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'fr', } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDveZc68ct7gaLomIZoFWQ-Ih3XArtlWpA",
+    authDomain: "inflouis-01.firebaseapp.com",
+    databaseURL: "https://inflouis-01-default-rtdb.firebaseio.com",
+    projectId: "inflouis-01",
+    storageBucket: "inflouis-01.appspot.com",
+    messagingSenderId: "195523650478",
+    appId: "1:195523650478:web:2a91d9f710887a219f9e3f",
+    measurementId: "G-B163KXDNXB"
+  }
 };
