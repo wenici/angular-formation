f2cabc17549cc44b9994b36512c25d605e59b276
wenici <weni.dev@gmail.com>
2022-10-13 18:57:18 +0000

    Update auth form

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/layout/**/*.{html,ts}",
+    "./src/components/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
